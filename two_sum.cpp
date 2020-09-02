a18b0256aa4f8372c337c11be13835279bda0a59
Jayasree77 <kjsree1998@gmail.com>
2020-09-02 22:33:30 +0530

    Leetcode Two_Sum

---
+++ two_sum.cpp
@@ -0,0 +1,17 @@
+//Leetcode
+//Problem Two Sum
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       unordered_map<int,int> umap;
+        for(int i=0;i<nums.size();i++){
+            int complement = target - nums[i];
+            if(umap.count(complement))
+                return {umap[complement],i};
+            else
+                umap[nums[i]] = i;
+        }
+        return {};
+    }
+};
