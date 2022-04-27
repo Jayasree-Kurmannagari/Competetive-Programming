4306c9fa478e4e12c9a4ff87cdf92772a469fe9c
Jayasree Kurmannagari <jayasree0708@gmail.com>
2022-04-27 22:25:42 +0530

    Find duplicate in an array of N+1 Integers

---
+++ Medium/Arrays,Vectors/finding_duplicate.cpp
@@ -0,0 +1,20 @@
+//Leetcode
+//https://leetcode.com/problems/find-the-duplicate-number/
+//Binary search
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int low = 1;
+        int high = nums.size()-1;
+        int mid;
+        while(low<high){
+            mid = (low+high)/2;
+            int count=0;
+            for(int num : nums) {if(num<=mid) count++;}
+            if(count>mid) high = mid;
+            else low = mid+1;
+        }
+        return low;
+    }
+};
